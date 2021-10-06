bfb22e164873f04b3be2089de0a1ffadc15a6da7
Yashashree Majalikar <yashmaja@gmail.com>
2021-10-06 11:01:53 -0400

    added methods

--- shoppingListLab/Program.cs
+++ shoppingListLab/Program.cs
@@ -8,64 +8,38 @@ namespace shoppingListLab
     {
         static void Main(string[] args)
         {
-            var comparer = StringComparer.OrdinalIgnoreCase;
-            Dictionary<string, double> items = new Dictionary<string, double>(comparer);
-            items.Add("Pringles", 1.99);
-            items.Add("IceBreaker Mints", 2.31);
-            items.Add("Red Bull", 3.39);
-            items.Add("Diet Pepsi", 2.50);
-            items.Add("Salted Pistachios", 4.49);
-            items.Add("Hot Cheeto Puffcorn", 2.00);
-            items.Add("Crunch Chocolate Bar", 3.50);
-            items.Add("Protein Bar", 3.00);
-            items.Add("Salt and Vinegar Kettle Chips", 2.29);
-            items.Add("Coffee", 1.50);
-
-            foreach (KeyValuePair<string, double> kvp in items)
-            {
-                Console.WriteLine($"{kvp.Key,-35}$ {kvp.Value:0.00}");
-            }
+            Console.WriteLine("Welcome to Yash's Market!");
 
             List<string> shoppingListItems = new List<string>();
             List<double> prices = new List<double>();
 
+            var comparer = StringComparer.OrdinalIgnoreCase;
+            Dictionary<string, double> items = new Dictionary<string, double>(comparer);
+            items.Add("A - Pringles", 1.99);
+            items.Add("B - IceBreaker Mints", 2.31);
+            items.Add("C - Red Bull", 3.39);
+            items.Add("D - Diet Pepsi", 2.50);
+            items.Add("E - Salted Pistachios", 4.49);
+            items.Add("F - Hot Cheeto Puffcorn", 2.00);
+            items.Add("G - Crunch Chocolate Bar", 3.50);
+            items.Add("H - Protein Bar", 3.00);
+            items.Add("I - Salt and Vinegar Kettle Chips", 2.29);
+            items.Add("J - Coffee", 1.50);
+            
             bool repeat = true;
 
             while (repeat)
             {
-                Console.Write("\nWhat would you like? Enter an item from list: ");
-                string input = Console.ReadLine();
-
-                bool keyExists = items.ContainsKey(input);
-
-                if (keyExists)
-                {
-                    foreach (KeyValuePair<string, double> kvp in items)
-                    {
-                        if (kvp.Key.ToLower() == input.ToLower())
-                        {
-                            Console.WriteLine($"\n{kvp.Key}............. $ {kvp.Value:0.00}");
-                            shoppingListItems.Add(kvp.Key);
-                            prices.Add(kvp.Value);
-                        }
-                    }
-
-                    repeat = Repeat();
-                }
-                else
-                {
-                    Console.WriteLine("\nInvalid input. Try again.\n");
-                }
+                PrintDictionary(items);
+                
+                string addItem = GetItemNum(items);
+                shoppingListItems.Add(addItem.Substring(3));
+                prices.Add(items[addItem]);
+                
+                repeat = Repeat();
             }
 
-            Console.WriteLine("\n\nHere is your receipt: ");
-            for (int i = 0; i < shoppingListItems.Count; i++)
-            {
-                Console.WriteLine($"{shoppingListItems[i], -50}$ {prices[i]:0.00}");
-            }
-
-            Console.WriteLine($"{"\nTotal: "}....................................$ {prices.Sum():0.00}");
-            Console.WriteLine($"{"The average price of your items is: "}.......$ {prices.Average():0.00}");
+            PrintReceipt(shoppingListItems, prices);
         }
 
         public static bool Repeat()
@@ -84,6 +58,87 @@ namespace shoppingListLab
                     return true;
                 }
                 else
+                {
+                    Console.WriteLine("\nInvalid input. Try again.\n");
+                }
+            }
+        }
+
+        public static void PrintDictionary(Dictionary<string, double> dict)
+        {
+            Console.WriteLine($"{"Item",-35} Price");
+            Console.WriteLine("================================================");
+            foreach (KeyValuePair<string, double> kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key,-35}$ {kvp.Value:0.00}");
+            }
+        }
+
+        public static string GetItem(Dictionary<string, double> dict)
+        {
+            while(true)
+            {
+                Console.Write("\nWhat would you like? Enter an item from list: ");
+                string input = Console.ReadLine();
+
+                bool keyExists = dict.ContainsKey(input);
+                if (keyExists)
+                {
+                    foreach (KeyValuePair<string, double> kvp in dict)
+                    {
+                        if (kvp.Key.ToLower() == input.ToLower())
+                        {
+                            Console.WriteLine($"\nAdding {kvp.Key}............. $ {kvp.Value:0.00}");
+                            input = kvp.Key;
+
+                        }
+                    }
+                    return input;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid input. Try again.\n");
+                }
+            }
+        }
+
+        static void PrintReceipt(List<string> itemNames, List<double> itemPrices)
+        {
+            Console.WriteLine("\n\nHere is your receipt: ");
+            for (int i = 0; i < itemNames.Count; i++)
+            {
+                Console.WriteLine($"{itemNames[i],-25}$ {itemPrices[i]:0.00}");
+            }
+
+            Console.WriteLine($"{"\nTotal: "}....................................$ {itemPrices.Sum():0.00}");
+            Console.WriteLine($"{"The average price of your items is: "}.......$ {itemPrices.Average():0.00}");
+        }
+
+        static string GetItemNum(Dictionary<string, double> numDict)
+        {
+            bool validInput = true;
+
+            while (true)
+            {
+                Console.Write("\nWhat would you like? Enter number of item (A-J): ");
+                string input = Console.ReadLine();
+
+                foreach (KeyValuePair<string, double> kvp in numDict)
+                {
+                    if (kvp.Key.ToUpper().Substring(0, 1) == input.ToUpper())
+                    {
+                        Console.WriteLine($"\nAdding {kvp.Key.Substring(3)}............. $ {kvp.Value:0.00}");
+                        input = kvp.Key;
+                        return input;
+                        //break;
+                    }
+                    else
+                    {
+                        validInput = false;
+                    }
+                }
+                
+                if (validInput == false)
                 {
                     Console.WriteLine("\nInvalid input. Try again.\n");
                 }
